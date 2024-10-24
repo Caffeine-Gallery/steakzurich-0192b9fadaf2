c3ade920df2e5dc439a8cd442fbc777b4f158d9e
sa-gems-github <sa-gems-github@dfinity.org>
2024-10-24 17:25:23 -0400

    0192c06b-27e4-777e-94b9-4b41d647aefe

--- src/declarations/backend/index.js
+++ src/declarations/backend/index.js
@@ -10,7 +10,7 @@ export { idlFactory } from "./backend.did.js";
  * beginning in dfx 0.15.0
  */
 export const canisterId =
-  'n64up-aaaaa-aaaab-qbi6q-cai';
+  'nfzik-2yaaa-aaaab-qbi4a-cai';
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
